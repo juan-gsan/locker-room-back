af937a8d30bb8a598529b7d444b388c1c1eaebe6
JuanG <juan.gsanchez13@gmail.com>
2023-06-26 19:07:55 +0200

    Create router user

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,7 +1,6 @@
 import { UserController } from '../controllers/user.controller.js';
 import { User } from '../entities/user.js';
 import { FileMiddleware } from '../middlewares/files.js';
-import { Validation } from '../middlewares/validation.js';
 import { Repo } from '../repository/repo.js';
 import { UserRepo } from '../repository/user.m.repo.js';
 import { Router as createRouter } from 'express';
@@ -13,7 +12,6 @@ debug('Executed');
 const repo: Repo<User> = new UserRepo();
 const controller = new UserController(repo);
 const fileStore = new FileMiddleware();
-const validation = new Validation();
 export const userRouter = createRouter();
 
 userRouter.get('/', controller.getAll.bind(controller));
@@ -21,7 +19,6 @@ userRouter.get('/:id', controller.getById.bind(controller));
 userRouter.post(
   '/register',
   fileStore.singleFileStore('avatar').bind(fileStore),
-  validation.registerValidation().bind(validation),
   fileStore.optimization.bind(fileStore),
   fileStore.saveImage.bind(fileStore),
   controller.register.bind(controller)
