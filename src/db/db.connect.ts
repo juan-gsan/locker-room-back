3e591a520454c7d52e37af9c9bcfe89c84ae4b57
JuanG <juan.gsanchez13@gmail.com>
2023-06-26 17:18:25 +0200

    Environment configs

--- src/db/db.connect.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import { db, password, user } from '../config.js';
-
-export const dbConnect = () => {
-  const uri = `mongodb+srv://${user}:${password}@cluster0.gsa5w92.mongodb.net/${db}?retryWrites=true&w=majority`;
-  return mongoose.connect(uri);
-};
