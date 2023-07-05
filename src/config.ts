bb87dd27f17233ff5b3f2e2bcbed913d6dee0861
JuanG <juan.gsanchez13@gmail.com>
2023-07-05 09:49:28 +0200

    Add firebase

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,12 @@ export const user = process.env.DB_USER;
 export const password = process.env.DB_PASSWORD;
 export const db = process.env.DB_NAME;
 export const secret = process.env.JWT_SECRET;
-export const firebaseApiKey = process.env.FIREBASE_APIKEY;
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDfQTwm5EPQ0FyqK4ho4423dZj-mYjZ8xo',
+  authDomain: 'w7ch5-e7d0e.firebaseapp.com',
+  projectId: 'w7ch5-e7d0e',
+  storageBucket: 'w7ch5-e7d0e.appspot.com',
+  messagingSenderId: '280404924862',
+  appId: '1:280404924862:web:544026cd9816a7eefb81ef',
+};
--- src/middlewares/files.ts
+++ src/middlewares/files.ts
@@ -3,44 +3,43 @@ import createDebug from 'debug';
 import path from 'path';
 import { NextFunction, Request, Response } from 'express';
 import { HttpError } from '../types/http.error.js';
-// TEMP import sharp from 'sharp';
+import sharp from 'sharp';
 import crypto from 'crypto';
+import { FireBase } from '../services/firebase.js';
 const debug = createDebug('FinalProject:FileMiddleware');
 
-// TEMP const optionsSets: {
-//   [key: string]: {
-//     width: number;
-//     height: number;
-//     fit: keyof sharp.FitEnum;
-//     position: string;
-//     quality: number;
-//   };
-// } = {
-//   register: {
-//     width: 300,
-//     height: 300,
-//     fit: 'cover',
-//     position: 'top',
-//     quality: 100,
-//   },
-// };
+const optionsSets: {
+  [key: string]: {
+    width: number;
+    height: number;
+    fit: keyof sharp.FitEnum;
+    position: string;
+    quality: number;
+  };
+} = {
+  register: {
+    width: 300,
+    height: 300,
+    fit: 'cover',
+    position: 'top',
+    quality: 100,
+  },
+};
 
 export class FileMiddleware {
   constructor() {
     debug('Instantiated');
   }
 
-  singleFileStore(fileName: string, fileSize = 8_000_000) {
-    console.log(fileName, fileSize);
+  singleFileStore(fileName = 'file', fileSize = 8_000_000) {
     const upload = multer({
       storage: multer.diskStorage({
-        destination: 'uploads',
+        destination: 'public/uploads',
         filename(req, file, callback) {
           const suffix = crypto.randomUUID();
           const extension = path.extname(file.originalname);
           const basename = path.basename(file.originalname, extension);
           const filename = `${basename}-${suffix}${extension}`;
-          debug('Called Multer');
           callback(null, filename);
         },
       }),
@@ -52,49 +51,50 @@ export class FileMiddleware {
     return middleware;
   }
 
-  // TEMP async optimization(req: Request, res: Response, next: NextFunction) {
-  //   try {
-  //     if (!req.file) throw new HttpError(406, 'Not Acceptable', 'Invalid file');
+  async optimization(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.file) {
+        throw new HttpError(406, 'Not Acceptable', 'Not valid image file');
+      }
 
-  //     const reqPath = req.path.split('/')[1];
-  //     const options = optionsSets[reqPath];
-  //     const fileName = req.file.filename;
-  //     const baseFileName = `${path.basename(fileName, path.extname(fileName))}`;
+      const reqPath = req.path.split('/')[1];
+      const options = optionsSets[reqPath];
+      const fileName = req.file.filename;
+      const baseFileName = `${path.basename(fileName, path.extname(fileName))}`;
 
-  //     const imageData = await sharp(path.join('uploads', fileName))
-  //       .resize(options.width, options.height, {
-  //         fit: options.fit,
-  //         position: options.position,
-  //       })
-  //       .webp({ quality: options.quality })
-  //       .toFormat('webp')
-  //       .toFile(path.join('uploads', `${baseFileName}_1.webp`));
+      const imageData = await sharp(path.join('public/uploads', fileName))
+        .resize(options.width, options.height, {
+          fit: options.fit,
+          position: options.position,
+        })
+        .webp({ quality: options.quality })
+        .toFormat('webp')
+        .toFile(path.join('public/uploads', `${baseFileName}.webp`));
 
-  //     req.file.originalname = req.file.path;
-  //     req.file.filename = `${baseFileName}.${imageData.format}`;
-  //     req.file.mimetype = `image/${imageData.format}`;
-  //     req.file.path = path.join('uploads', req.file.filename);
-  //     req.file.size = imageData.size;
+      req.file.originalname = req.file.path;
+      req.file.filename = `${baseFileName}.${imageData.format}`;
+      req.file.mimetype = `image/${imageData.format}`;
+      req.file.path = path.join('public/uploads', req.file.filename);
+      req.file.size = imageData.size;
 
-  //     next();
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
 
   saveImage = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      debug('Called saveImage');
-      if (!req.file) {
-        throw new HttpError(406, 'Not Acceptable', 'Invalid image file');
-      }
+      if (!req.file)
+        throw new HttpError(406, 'Not Acceptable', 'Not valid image file');
 
       const userImage = req.file.filename;
-      const imagePath = path.join('uploads', userImage);
+      const firebase = new FireBase();
+      const backupImage = await firebase.uploadFile(userImage);
 
       req.body[req.file.fieldname] = {
         urlOriginal: req.file.originalname,
-        url: imagePath,
+        url: backupImage,
         mimetype: req.file.mimetype,
         size: req.file.size,
       };
